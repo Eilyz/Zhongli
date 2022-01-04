920572690597743b7a62ded6ae3c376e1bfac628
Zyla <erencana12@gmail.com>
2022-01-04 12:49:29 +0300

    Update pray.js
    Change image link

--- src/commands/Server/pray.js
+++ src/commands/Server/pray.js
@@ -18,7 +18,7 @@ export default {
         new MessageEmbed({
           color: 'YELLOW',
           title: 'May your solemn invocation be favored by Rex Lapis\' benevolence',
-          image: { url: 'https://ibb.co/yqggfpd' },
+          image: { url: 'https://ibb.co/yqggfpd.gif' },
         }),
       ],
     })
