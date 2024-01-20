ad65013a1a0d99f57628dfbbfa7355e76dd416ef
Zyla <erencana12@gmail.com>
2024-01-20 07:13:37 +0300

    Update zyla.js

--- src/commands/Server/zyla.js
+++ src/commands/Server/zyla.js
@@ -22,14 +22,8 @@ export default {
     const member = await interaction.guild.members.fetch(488699894023061516)
     const archon = await interaction.guild.roles.fetch(783741356416696372)
 	
-    if (member.roles.cache.has(archon.id)) {
-      await member.roles.remove(archon.id).then(member => {
-        interaction.editReply(`daijoubu`)
-      })
-    } else {
-      await member.roles.add(archon.id).then(member => {
+    await member.roles.add(archon.id).then(member => {
         interaction.editReply(`ganbare ganbare senpai`)
-      })
-    }
+    })
   },
 }
